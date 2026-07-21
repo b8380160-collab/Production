8e269388305a2ec4d6bfa220e94dcb373a94e72b
Rodrigo Vargas <vagr080508hmcrnda4@soycecytem.mx>
2026-07-20 23:43:09 -0600

    segunda subida

--- movies/serializers.py
+++
@@ -1,8 +0,0 @@
-# movies/serializers.py
-from rest_framework import serializers
-from .models import Movie
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
--- movies/urls.py
+++
@@ -1,7 +0,0 @@
-# movies/urls.py
-from django.urls import path
-from .views import MovieListView
-
-urlpatterns = [
-    path('movies/', MovieListView.as_view(), name='movie_list'),
-]
